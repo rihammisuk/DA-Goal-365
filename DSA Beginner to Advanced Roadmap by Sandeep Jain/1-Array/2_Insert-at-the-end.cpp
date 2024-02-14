6b3f62c03a5682906c32513fa5f0eeb74b3ff1dc
rihammisuk <riham659misuk@gmail.com>
2024-02-14 14:51:31 +0600

    =kichui hoynai

---
+++ DSA Beginner to Advanced Roadmap by Sandeep Jain/1-Array/2_Insert-at-the-end.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int insertSorted(int arr[], int n, int key, int capacity)
+{
+    // Cannot insert more elements if n is
+    // already more than or equal to capacity
+    if (n >= capacity)
+        return n;
+
+    arr[n] = key;
+    return (n + 1);
+}
+
+int main()
+{
+    int arr[20] = { 12, 16, 20, 40, 50, 70 };
+    int capacity = sizeof(arr) / sizeof(arr[0]);
+    int n = 6;
+    int i, key = 26;
+
+    cout << "Before Insertion: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    // Inserting key
+    n = insertSorted(arr, n, key, capacity);
+
+    cout << "\nAfter Insertion: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
